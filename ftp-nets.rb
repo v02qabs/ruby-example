c04c069aad216b8f44f2315f376e99074c24a689
v02qabs <takesue090@gmail.com>
2024-08-18 21:21:34 +0900

    ftp-send-file

--- ftp-nets.rb
+++ ftp-nets.rb
@@ -8,6 +8,13 @@ pass = gets.chomp
 ftp_server = server  # サーバーのホスト名またはIPアドレス
 ftp_user =   user     # FTPユーザー名
 ftp_password = pass  # FTPパスワード
+require 'net/ftp'
+
+# ローカルのファイルパス
+local_file_path = 'hello.txt'    # アップロードするファイルのパス
+
+# リモートのディレクトリ
+remote_directory = '/public_html/.'  # 移動したいサーバー側のディレクトリ
 
 begin
   # FTPオブジェクトを作成
@@ -19,13 +26,15 @@ begin
   # ログイン
   ftp.login(ftp_user, ftp_password)
 
-  puts "ログインに成功しました"
+  # 任意のディレクトリへ移動
+  ftp.chdir(remote_directory)
 
-  # 必要な操作をここに追加
-  # 例: ファイル一覧を取得
-  files = ftp.list
-  puts "サーバー上のファイル一覧:"
-  puts files
+  puts "ディレクトリ #{remote_directory} に移動しました"
+
+  # ファイルをアップロード
+  ftp.putbinaryfile(local_file_path)
+
+  puts "ファイル #{local_file_path} をアップロードしました"
 
   # FTPセッションを終了
   ftp.close
